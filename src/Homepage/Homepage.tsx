f277b0b441aba5442e0776d103ae6e544ac9d239
Tuszmak <daniel.pinter02@gmail.com>
2025-02-21 10:59:05 +0100

    fix: fix homepage menubar

--- src/Homepage/Homepage.tsx
+++ src/Homepage/Homepage.tsx
@@ -2,17 +2,15 @@ import { Button } from '@/components/ui/button';
 
 export default function Homepage() {
   return (
-    <div>
-      <div className='flex flex-col items-center gap-9'>
-        <h1 className='text-6xl'>Bingo Generator</h1>
-        <p className='font-bold text-2xl'>
-          The only bingo generator you'll ever need.
-        </p>
+    <div className='flex flex-col items-center gap-9'>
+      <h1 className='text-6xl'>Bingo Generator</h1>
+      <p className='font-bold text-2xl'>
+        The only bingo generator you'll ever need.
+      </p>
 
-        <a href='/setup'>
-          <Button>Get Started</Button>
-        </a>
-      </div>
+      <a href='/setup'>
+        <Button>Get Started</Button>
+      </a>
     </div>
   );
 }
